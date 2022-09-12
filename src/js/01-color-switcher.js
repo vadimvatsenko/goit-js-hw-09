816a9dc402c0e7ae5ee3c73a40721af0b90e835b
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-09-12 20:05:31 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+}
 
+refs.startBtn.addEventListener("click", clickStart);
+refs.stopBtn.addEventListener("click", clickStop);
+
+function clickStart() {
+    refs.startBtn.disabled = true;
+    timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function clickStop() {
+  clearTimeout(timerId);
+  refs.body.style.backgroundColor = 'transparent';
+  refs.startBtn.disabled = false;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
